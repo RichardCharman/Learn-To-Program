2582edb699b2ff89654e1f464d312cdcec6e89de
Richard Charman <richardcharman@live.com>
2015-06-26 21:42:06 +0000

    Chapter 1 - Numbers

---
+++ chris-pine-age.rb
@@ -0,0 +1,7 @@
+puts "Chris Pine is 1204000000 seconds old."
+days = (1204000000/86400)
+puts "Which equates to " + days.to_s + " days old."
+months = (days/365)*12
+puts "Or " + months.to_s + " months old."
+years = (months/12)
+puts "Or " + years.to_s + " years old."
---
+++ how-old.rb
@@ -0,0 +1,6 @@
+puts "How old are you?"
+age = gets.chomp
+puts "Your age is "+age
+puts "You have lived..."
+puts 86400*365*age.to_i
+puts "...seconds."
